769f5994fe3528c297cd8c727f38f863486367bf
Joan Martinez Moreno <sebastian.martinez@sii-group.co>
2025-04-10 18:10:32 -0500

    Create account

---
+++ go-api/internal/app/bank/bankRepository.go
@@ -0,0 +1,16 @@
+package bank
+
+import (
+	"context"
+	"go-api/internal/domain"
+)
+
+type BankRepository interface {
+	CreateUser(ctx context.Context, user *domain.User) error
+	GetUserByAccountNumber(ctx context.Context, numeroCuenta string) (*domain.User, error)
+	UpdateUserBalance(ctx context.Context, numeroCuenta string, newBalance float64) error
+
+	CreateTransaction(ctx context.Context, transaction *domain.Transaction) error
+	GetTransactionsByAccount(ctx context.Context, numeroCuenta string) ([]domain.Transaction, error)
+	GetTransactionsByAccountNumber(ctx context.Context, accountNumber string) ([]domain.Transaction, error)
+}
