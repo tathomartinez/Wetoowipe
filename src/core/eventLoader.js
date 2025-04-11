7971a57004fed1d659b531ed69b8f196c59e7a97
Joan Martinez Moreno <sebastian.martinez@sii-group.co>
2025-04-10 22:18:47 -0500

    Add voice state update listener and welcome message for new members

--- src/core/eventLoader.js
+++ src/core/eventLoader.js
@@ -1,16 +1,19 @@
-const fs = require('node:fs');
-const path = require('node:path');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = function loadEvents(client) {
-	const eventsPath = path.join(__dirname, '../events');
-	const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+module.exports = (client) => {
+    const eventsPath = path.join(__dirname, '../events');
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-	for (const file of eventFiles) {
-		const event = require(path.join(eventsPath, file));
-		if (event.once) {
-			client.once(event.name, (...args) => event.execute(...args));
-		} else {
-			client.on(event.name, (...args) => event.execute(...args));
-		}
-	}
+    for (const file of eventFiles) {
+        const filePath = path.join(eventsPath, file);
+        const event = require(filePath);
+		console.log(`Cargando evento: ${event.name}`); // Agrega este log
+
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args));
+        }
+    }
 };
