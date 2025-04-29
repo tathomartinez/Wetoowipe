ffa8d640c517bf531aebeee2a56cda69e40ae492
Joan Martinez Moreno <sebastian.martinez@sii-group.co>
2025-04-28 21:34:34 -0500

    Implement VoiceStateUpdate event handler with music player integration

---
+++ bot/src/events/listenerGoinVoiceChat.ts
@@ -0,0 +1,40 @@
+import { Events, VoiceState, Guild, VoiceChannel } from 'discord.js';
+import { musicPlayer } from '../core/musicPlayer'; // Importar la función musicPlayer
+import AudioPaths from '../audio/audioPaths'; // Importar el "enum"
+
+export default {
+    name: Events.VoiceStateUpdate,
+    once: false,
+    async execute(oldState: VoiceState, newState: VoiceState): Promise<void> {
+        try {
+            // Salir si el usuario no se conectó a un canal de voz
+            const botId = '868646244380192818'; // ID del bot
+            if (newState.member?.id === botId) {
+                return;
+            }
+
+            if (oldState.channel || !newState.channel) {
+                return;
+            }
+
+            console.log(`${newState.member?.user.tag} se conectó al canal de voz: ${newState.channel.name}`);
+
+            // Salir si no es el usuario específico
+            // const specificUserId = '214877856994557952';
+            // if (newState.member?.id !== specificUserId) {
+            //     return;
+            // }
+
+            console.log(`El usuario específico ${newState.member?.user.tag} se conectó al canal de voz: ${newState.channel.name}`);
+
+            // Ejecutar musicPlayer
+            await musicPlayer({
+                voiceChannel: newState.channel as VoiceChannel,
+                guild: newState.guild as Guild,
+                audioPath: AudioPaths.BIENVENIDO,
+            });
+        } catch (error) {
+            console.error('Error en el evento VoiceStateUpdate:', error);
+        }
+    },
+};
